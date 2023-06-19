240dc1c8459e37eeb24711fee3bbcdfb8de59a86
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-06-19 20:45:48 +0300

    тест

--- meddlwares/validation.js
+++ meddlwares/validation.js
@@ -30,7 +30,7 @@ const validationUpdateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
     about: Joi.string().min(2).max(30).required(),
-    avatar: Joi.string().required().custom(validationUrl),
+    avatar: Joi.string().pattern(/http(s)?:\/\/(www.)?[a-z0-9\.\-]+\/[a-z0-9\.\-_~:\/?#\[\]@!$&'()*+,;=]+/),
   }),
 });
 
