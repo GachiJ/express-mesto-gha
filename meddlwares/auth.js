989e88bcf475db4254fa64a9669057eb026eef34
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-06-19 21:10:00 +0300

    тест

--- meddlwares/auth.js
+++ meddlwares/auth.js
@@ -5,6 +5,11 @@ const auth = (req, res, next) => {
 
 
   const token = req.cookies.jwt;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Требуется авторизация' });
+  }
+
   let payload;
 
   try {
