c90b86f0b1d83a027428295dadf9811c53073dc5
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-06-19 13:30:52 +0300

    Реализовал вторую часть бэка для приложения Mesto без авторизации и регистрации

--- routes/users.js
+++ routes/users.js
@@ -9,19 +9,19 @@ const auth = require('../meddlwares/auth')
 const { getUsers, getUsersById, createUser, upDateUser, upDateUserAvatar, login, getUserInfo, } = require('../controllers/users');
 
 
-
-
-router.use(auth);
-
 router.get('/users', getUsers);
 router.get('/users/:id', getUsersById);
 router.patch('/users/me', upDateUser);
 router.get('/users/me', getUserInfo);
 router.patch('/users/me/avatar', upDateUserAvatar);
 
-// Маршруты регистрации и логина
 router.post('/signin', validationLogin, login);
 router.post('/signup', validationCreatUser, createUser);
+
+router.use(auth);
+
+
+
 
 
 
